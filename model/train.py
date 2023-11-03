f1f1b46542b836e07fb29214209dbc75aa5a59c0
ChristianReyes11 <christianamaro2000@gmail.com>
2023-11-03 08:25:52 -0600

    ml_ci_cd

--- model/train.py
+++ model/train.py
@@ -6,7 +6,7 @@ from joblib import dump
 import pandas as pd
 import pathlib
 
-df = pd.read_csv(pathlib.Path('../data/fraud.csv'))
+df = pd.read_csv(pathlib.Path('./data/fraud.csv'))
 y = df.pop('isFraud')
 X = df
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2)
